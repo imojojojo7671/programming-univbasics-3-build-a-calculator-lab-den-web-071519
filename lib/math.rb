10da58babe68d0ebe94507890cf6a4b552d1668b
Drew Peterson <drew.a.peterson@hotmail.com>
2019-07-03 21:55:36 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -15,7 +15,8 @@ def multiplication(num1, num2)
 end
 
 def modulo(num1, num2)
-  p ((num1.to_f / num2.to_f) - (num1 - num2))* num2
+#  p ((num1.to_f / num2.to_f) - (num1 - num2))* num2
+  p num1.to_f / num2.to_f
 end
 
 def square_root(num)
