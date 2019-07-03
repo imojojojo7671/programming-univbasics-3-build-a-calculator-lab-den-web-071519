3aeaaae09e8e915349f713b5ef2aebe9482948ad
Drew Peterson <drew.a.peterson@hotmail.com>
2019-07-03 22:07:28 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -14,11 +14,12 @@ def multiplication(num1, num2)
   p num1 * num2
 end
 
-def modulo(num1, num2)
+def modulo(num1, num2) 
+  #required to get the remainder of arguments
   raw_float = ((num1.to_f / num2.to_f) - (num1 / num2)) * num2
-  p raw_float.round(1)
+  p raw_float.round(1) #was coming up 3.99999~
 end
 
 def square_root(num)
-  p Math.sqrt(num)
+  p Math.sqrt(num) #square root
 end
