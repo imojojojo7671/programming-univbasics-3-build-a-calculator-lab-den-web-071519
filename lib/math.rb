9929e861d99ee3f3de846fbf334dc39ae2ad5de3
Drew Peterson <drew.a.peterson@hotmail.com>
2019-07-03 21:30:48 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,24 @@
 def addition(num1, num2)
-
+ p num1 + num2
 end
 
 def subtraction(num1, num2)
-
+  p num1 - num2
 end
 
 def division(num1, num2)
-
+  p num1 / num2 
 end
 
 def multiplication(num1, num2)
-
+  p num1 * num2
 end
 
 def modulo(num1, num2)
-
+  raw_number = num1 / num2
+  p raw_number - raw_number.to_i
 end
 
 def square_root(num)
-
+  p math.sqrt(num)
 end
